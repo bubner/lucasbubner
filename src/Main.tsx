a1089a84747325596f7491a188c6ec1486a9a8bb
Lucas Bubner <lkbubner@proton.me>
2023-02-05 22:54:21 +1030

    Smooth animation integration

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,21 +3,36 @@
  * @author Lucas Bubner, 2023
  */
 import { createRef, useEffect, useState } from "react";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 import "./Main.css";
 
 function Main() {
+    const [isExplained, setIsExplained] = useState(false);
     const winRef = createRef<HTMLDivElement>();
     useEffect(() => {
         winRef.current?.scrollIntoView({ behavior: "smooth" });
+        setTimeout(() => {
+            setIsExplained(true);
+        }, 2000);
     }, []);
 
     return (
         <>
             <div className="text-container">
-                <p className="text-line">I build applications,</p>
-                <p className="text-line">for the future.</p>
+                <p className="text-line">I am a self-taught,</p>
+                <p className="text-line">young software developer.</p>
             </div>
             <div ref={winRef} />
+            {isExplained && (
+                <>
+                    <img id="extd-bg" src="/swl.png" />
+                    <div className="text-element">
+                        <AnimationOnScroll animateIn="animate__fadeInLeft" animateOut="animate__fadeOutLeft">
+                            <p>Powered by intuition, <br /> <br /> <br /> influenced by soul.</p>
+                        </AnimationOnScroll>
+                    </div>
+                </>
+            )}
         </>
     );
 }
