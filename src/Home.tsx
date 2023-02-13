b39b940636b7a27413dbdaeaf0f4c41a5ee55a9c
Lucas Bubner <lkbubner@proton.me>
2023-02-13 19:01:43 +1030

    Improved transitions

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,14 +3,20 @@
  * @author Lucas Bubner, 2023
  */
 import Navbar from "./Navbar";
+import { motion } from "framer-motion";
 import "./Home.css";
 
 function Home() {
     return (
-        <>
+        <motion.div
+            initial={{ opacity: location.pathname !== "/i" ? 1 : 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+        >
             <Navbar />
             <div className="content-bg" />
-        </>
+        </motion.div>
     );
 }
 
