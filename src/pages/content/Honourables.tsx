402a8904d28e3e8c7afa748f3be798964ef91261
Lucas Bubner <lkbubner@proton.me>
2023-11-01 16:52:57 +1030

    Formatting changes

--- src/pages/content/Honourables.tsx
+++ src/pages/content/Honourables.tsx
@@ -15,8 +15,8 @@ function Honourables() {
                 <Box src="/iawards.png" size={50}>
                     <h1>SA/NT iAwards Winner</h1>
                     <p>
-                        <b>Australian Information Industry Association</b> <br /> Winner of the 2023 SA/NT Student &
-                        Education category at the iAwards, developing the Bunyip Bellower project.
+                        <b>Australian Information Industry Association</b> <br /> <b>Winner</b> of the 2023 SA/NT
+                        Student &amp; Education category at the iAwards, developing the Bunyip Bellower project.
                         <br />
                         <a
                             className="internallink"
@@ -30,9 +30,9 @@ function Honourables() {
                 <Box src="/iawards.png" size={50}>
                     <h1>National iAwards Merit</h1>
                     <p>
-                        <b>Australian Information Industry Association</b> <br /> Merit recipient of the 2023 National
-                        Student &amp; Education category at the iAwards, undergoing Stage II judging against other
-                        prestigious projects at a <b>tertiary level</b>.
+                        <b>Australian Information Industry Association</b> <br /> <b>Merit recipient</b> of the 2023
+                        Student &amp; Education category at the <b>national iAwards</b>, undergoing Stage II judging
+                        against other prestigious projects at a <b>tertiary level</b>.
                         <br />
                         <a
                             className="internallink"
@@ -62,16 +62,16 @@ function Honourables() {
                     <h1>Year 8/9 Dux of 2021/2022</h1>
                     <p>
                         <b>Murray Bridge High School</b> <br /> <b>Multi-year</b> Dux student at MBHS; highest achieving
-                        academic student for grade point average across multiple years. Striving student in
+                        <b> academic student</b> for grade point average across multiple years. Striving student in
                         <b> Specialist and Methods Mathematics</b>, <b>Physics</b>, and <b>Digital Technology</b>.
                     </p>
                 </Box>
                 <Box src="/bunyipst.png" size={45}>
-                    <h1>Murray Bridge Bunyips Captain</h1>
+                    <h1>MBHS Student Robotics Club Captain</h1>
                     <p>
-                        <b>May 2022 - Present</b> <br /> Club captain of multiple registered FIRST Tech Challenge teams.
-                        Presented and spoke to councils and entities for needs and requests, representing the club as a
-                        whole.
+                        <b>May 2022 - Present</b> <br /> Club captain of <b>FIRST Tech Challenge</b> teams 15215, 22407,
+                        and 24736. <b>Led</b> the club push <b>new frontiers</b>, in both <b>software programming</b>{" "}
+                        and <b>community outreach</b>.
                         <br />
                         <a
                             className="internallink"
