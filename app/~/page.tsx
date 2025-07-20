3e03f7dfaa958beeece1da1a27119e3a1f77c26d
Lucas Bubner <lkbubner@proton.me>
2025-07-20 15:32:04 +0930

    Entry animation and WT display
    Need to reconstruct the Box class to use in all pages and decide what info will be shown on the front page, thinking CV

--- app/~/page.tsx
+++ app/~/page.tsx
@@ -2,17 +2,27 @@ import { Suspense } from "react";
 import GitHubRepoTree from "../components/info-pages/home/GitHubRepoTree";
 import LoadingWheel from "../components/info-pages/LoadingWheel";
 import MinBox from "../components/info-pages/MinBox";
+import Image from "next/image";
+import WakaTime from "../components/info-pages/home/WakaTime";
+import entryIncrement from "../components/info-pages/entry-timing";
 
 export default function Home() {
+    const iter = entryIncrement(0.2);
     return (
-        <div className="w-full md:w-3/4 h-full flex items-center justify-center flex-col md:flex-row">
-            <div className="w-full md:w-1/2 flex flex-col">
-                <MinBox>WakaTime</MinBox>
-                <MinBox>Contacts</MinBox>
-                <MinBox>CV minibox</MinBox>
+        <div className="w-full 2xl:w-3/4 h-full flex items-center justify-center flex-col 2xl:flex-row">
+            <div className="w-full sm:w-[700px] flex flex-col">
+                <MinBox initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: iter.next().value!!, type: "spring", damping: 20, stiffness: 200 }}>
+                    CV mini
+                </MinBox>
+                <MinBox initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: iter.next().value!!, type: "spring", damping: 20, stiffness: 200 }}>
+                    <WakaTime />
+                </MinBox>
+                <MinBox initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: iter.next().value!!, type: "spring", damping: 20, stiffness: 200 }}>
+                    Contacts
+                </MinBox>
             </div>
-            <div className="w-full md:w-1/2">
-                <MinBox initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0, duration: 1 }}>
+            <div className="w-full 2xl:w-1/2">
+                <MinBox initial={{ y: 20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: iter.next().value!!, type: "spring", damping: 20, stiffness: 200 }}>
                     <Suspense fallback={<LoadingWheel containerHeight="33vh" />}>
                         <GitHubRepoTree />
                     </Suspense>
