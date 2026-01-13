8bf4b0236f9f5ed995a4ebd228528def02a661ff
Lucas Bubner <lkbubner@proton.me>
2026-01-13 16:02:23 +1030

    Complete My Projects

--- components/info-pages/showcase/MyProjects.tsx
+++ components/info-pages/showcase/MyProjects.tsx
@@ -1,13 +1,37 @@
-import { Bubner, BunyipBellower, Bunyips, CPlusPlus, FusionChess, Java, JavaCup, JsDos, MDBunyip, MindOfCeo, Mine, Minecraft, MinecraftSb, Pulse, Python, RoboRegistry, RoboticsLogos, Unity, UTTT, VintageStory, ZeusMonitor } from "@/images";
+import {
+    Bubner,
+    BunyipBellower,
+    Bunyips,
+    CPlusPlus,
+    FusionChess,
+    Java,
+    JavaCup,
+    JsDos,
+    MDBunyip,
+    MindOfCeo,
+    Mine,
+    Minecraft,
+    MinecraftSb,
+    Pulse,
+    Python,
+    RoboRegistry,
+    RoboticsLogos,
+    Unity,
+    UTTT,
+    VintageStory,
+    ZeusMonitor,
+} from "@/images";
 import Box from "../Box";
 import Image, { StaticImageData } from "next/image";
 import SoundLink from "@/components/SoundLink";
+import { Children } from "react";
+import { shuffle } from "@/lib/util";
 
 function Project({ icon, title, href }: { icon: StaticImageData; title: string; href: string }) {
     return (
         <SoundLink target="_blank" href={href} className="flex flex-col m-3 gap-1 items-center justify-center group relative">
             <div className="group-hover:blur-sm transition duration-300 flex flex-col items-center">
-                <Image src={icon} alt={title} width={70} height={70} className="w-[70px] h-[70px] p-1" />
+                <Image src={icon} alt={title} width={70} height={70} className="w-[70px] h-[70px] p-1 object-contain" />
                 <div className="flex gap-1">
                     <span className="text-xs select-none font-extrabold text-blue-300/50 underline">{title}</span>
                 </div>
@@ -19,14 +43,14 @@ function Project({ icon, title, href }: { icon: StaticImageData; title: string; 
     );
 }
 
-function ProjectGroup({ category, description, children }: { category: string; description: string; children: React.ReactNode }) {
+function ShuffledProjectGroup({ category, description, children }: { category: string; description: string; children: React.ReactNode }) {
     return (
         <div className="bg-black/40 w-full rounded-xl flex justify-between items-center">
-            <div className="pl-4 text-left">
+            <div className="pl-4 text-left *:w-max">
                 <h3 className="font-bold text-xl">{category}</h3>
                 <div className="text-sm">{description}</div>
             </div>
-            <div className="flex pr-4">{children}</div>
+            <div className="flex flex-wrap items-center justify-center pr-4">{shuffle(Children.toArray(children))}</div>
         </div>
     );
 }
@@ -36,7 +60,7 @@ export default function MyProjects({ entryDelay }: { entryDelay: number }) {
         <Box entryDelay={entryDelay}>
             <h1>My Projects</h1>
             <div className="flex flex-col w-full gap-1 h-96 overflow-y-auto">
-                <ProjectGroup category="Games" description="Desktop and web video games">
+                <ShuffledProjectGroup category="Games" description="Desktop and web video games">
                     <Project icon={FusionChess} title="Fusion Chess" href="https://git.bubner.me/FusionChess" />
                     <Project icon={Unity} title="Clusterbyte" href="https://git.bubner.me/Clusterbyte" />
                     <Project icon={Unity} title="Tanks" href="https://git.bubner.me/Tanks" />
@@ -44,15 +68,15 @@ export default function MyProjects({ entryDelay }: { entryDelay: number }) {
                     <Project icon={MindOfCeo} title="Mind" href="https://git.bubner.me/Mind" />
                     <Project icon={Mine} title="Minesweeper AI" href="https://git.bubner.me/MinesweeperAI" />
                     <Project icon={UTTT} title="Ultimate Tic Tac Toe" href="https://git.bubner.me/UltimateTicTacToe" />
-                </ProjectGroup>
-                <ProjectGroup category="Visualisation" description="Data transformation and abstraction">
+                </ShuffledProjectGroup>
+                <ShuffledProjectGroup category="Visualisation" description="Data transformation and abstraction">
                     <Project icon={ZeusMonitor} title="Zeus Monitor" href="https://git.bubner.me/ZeusMonitor" />
                     <Project icon={JavaCup} title="R3 Sim" href="https://git.bubner.me/r3sim" />
                     <Project icon={JavaCup} title="Hill Cipher" href="https://git.bubner.me/hillcipher-lib" />
                     <Project icon={JavaCup} title="Bézier" href="https://git.bubner.me/Bezier" />
                     <Project icon={JavaCup} title="BunyipsLib Virtual Robot" href="https://git.bubner.me/BunyipsLib_VirtualRobot" />
-                </ProjectGroup>
-                <ProjectGroup category="Websites" description="Web designed projects">
+                </ShuffledProjectGroup>
+                <ShuffledProjectGroup category="Websites" description="Web designed projects">
                     <Project icon={RoboRegistry} title="RoboRegistry" href="https://git.bubner.me/RoboRegistry" />
                     <Project icon={Bubner} title="CV" href="https://git.bubner.me/cv" />
                     <Project icon={Bubner} title="bubner.me" href="https://git.bubner.me/lucasbubner" />
@@ -60,31 +84,31 @@ export default function MyProjects({ entryDelay }: { entryDelay: number }) {
                     <Project icon={BunyipBellower} title="Bunyip Bellower" href="https://git.bubner.me/BunyipBellower" />
                     <Project icon={MDBunyip} title="BunyipsLib for Rookies" href="https://git.bubner.me/bunyipslib-for-rookies" />
                     <Project icon={FusionChess} title="Fusion Chess" href="https://git.bubner.me/FusionChess" />
-                </ProjectGroup>
-                <ProjectGroup category="Libraries" description="Collections of common code">
+                </ShuffledProjectGroup>
+                <ShuffledProjectGroup category="Libraries" description="Collections of common code">
                     <Project icon={Bunyips} title="BunyipsLib" href="https://git.bubner.me/BunyipsLib" />
                     <Project icon={CPlusPlus} title="Duino" href="https://git.bubner.me/Arduino-TouchScreen-Joystick" />
                     <Project icon={JavaCup} title="Hill Cipher" href="https://git.bubner.me/hillcipher-lib" />
-                </ProjectGroup>
-                <ProjectGroup category="APIs" description="Application programming interfaces">
+                </ShuffledProjectGroup>
+                <ShuffledProjectGroup category="APIs" description="Application programming interfaces">
                     <Project icon={Python} title="FIRST® Teams" href="https://git.bubner.me/FIRSTTeamAPI" />
                     <Project icon={Python} title="Profanity" href="https://git.bubner.me/ProfanityAPI" />
                     <Project icon={Python} title="BunyipsLib Wiki Search" href="https://git.bubner.me/BunyipsLib.wiki.search" />
-                </ProjectGroup>
-                <ProjectGroup category="Mods" description="Minecraft and Vintage Story game mods">
+                </ShuffledProjectGroup>
+                <ShuffledProjectGroup category="Mods" description="Minecraft and Vintage Story game mods">
                     <Project icon={VintageStory} title="Distance Displayer" href="https://git.bubner.me/DistanceDisplayer" />
                     <Project icon={MinecraftSb} title="Ping Offset Miner" href="https://git.bubner.me/PingOffsetMiner" />
                     <Project icon={Minecraft} title="Move Tooltip" href="https://git.bubner.me/movetooltip" />
                     <Project icon={MinecraftSb} title="Stats Ring" href="https://git.bubner.me/StatsRing" />
                     <Project icon={MinecraftSb} title="SkyBlock AFK" href="https://git.bubner.me/sbafk-forge" />
-                </ProjectGroup>
-                <ProjectGroup category="Hardware" description="Integrated devices and embedded">
+                </ShuffledProjectGroup>
+                <ShuffledProjectGroup category="Hardware" description="Integrated devices and embedded">
                     <Project icon={RoboticsLogos} title="FTC" href="https://git.bubner.me/BunyipsFTC" />
                     <Project icon={Bunyips} title="BunyipsLib" href="https://git.bubner.me/BunyipsLib" />
                     <Project icon={CPlusPlus} title="Duino" href="https://git.bubner.me/Arduino-TouchScreen-Joystick" />
                     <Project icon={Python} title="TI-84 Python" href="https://git.bubner.me/ti84-plus-ce-python" />
                     <Project icon={CPlusPlus} title="Arduino Cookie Clicker" href="https://git.bubner.me/Arduino-CookieClicker" />
-                </ProjectGroup>
+                </ShuffledProjectGroup>
             </div>
         </Box>
     );
